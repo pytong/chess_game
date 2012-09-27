eb5edda4d65fcc1e241bd67371daf746ab3e74c4
Puiyee Tong <ptong@blurb.com>
2012-09-27 08:47:52 -0700

    Added test cases to test black pawn initialization

--- chess_spec/chess_piece_spec.rb
+++ chess_spec/chess_piece_spec.rb
@@ -5,5 +5,9 @@ describe ChessPiece do
     pawn = ChessPiece.new(:WHITE, :PAWN)
     pawn.color.should == :WHITE
     pawn.type.should == :PAWN
+    
+    pawn = ChessPiece.new(:BLACK, :PAWN)
+    pawn.color.should == :BLACK
+    pawn.type.should == :PAWN
   end
 end
